7ad6a968a74a322ee34b5caf74ce0490ad929abd
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-01-03 16:24:16 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,28 @@
 // declare your functions here...
+$(document).ready(function () {
+  function paragraphSelector() {
+    $('p')
+
+  }
+
+  function lastImageSelector() {
+    $('img:last')
+
+  }
+
+function ninjaBabySelector() {
+
+  $('#baby-ninja')
+}
+
+function divSelector() {
+  $('.pics')
+
+}
+
+function firstListItem() {
+  $('div ul:first-child');
+
+}
+
+})
